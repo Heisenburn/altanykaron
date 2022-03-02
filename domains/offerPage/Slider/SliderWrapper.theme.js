a3bad74a0043ecf37f2277ce30be1f273de93fb9
Marcin Rudnik <marcinrudnik1996@gmail.com>
2022-03-02 14:55:28 +0100

    Update SliderWrapper.theme.js

--- domains/offerPage/Slider/SliderWrapper.theme.js
+++ domains/offerPage/Slider/SliderWrapper.theme.js
@@ -6,6 +6,24 @@ const SliderWrapper = styled.div`
   img {
     background-color: #c0c1c926;
   }
+
+  .fullscreen {
+    .image-gallery-fullscreen-button {
+      svg {
+        display: none;
+      }
+      &::after {
+        content: "\\2715";
+        position: absolute;
+        bottom: 85vh;
+        right: 24px;
+        color: white;
+        font-size: 40px;
+        font-weight: 500;
+        line-height: 1;
+      }
+    }
+  }
 `;
 
 export default SliderWrapper;
