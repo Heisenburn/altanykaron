cecfd9ed06da8c6244b99d4b1030587194e05ff5
Marcin Rudnik <marcinrudnik1996@gmail.com>
2022-03-03 14:40:13 +0100

    fix staticPaths issue

--- pages/oferta/[oferta].js
+++ pages/oferta/[oferta].js
@@ -14,7 +14,7 @@ export async function getStaticProps({ params }) {
   const { oferta } = params;
   const idFromURL = oferta.split("-").pop();
   const fs = require("fs");
-  const dir = `./public/images/offers/${idFromURL.toLowerCase()}`;
+  const dir = `./public/images/offers/${idFromURL.toUpperCase()}`;
 
   const availableImagesInDirectory = fs.readdirSync(dir);
   return {
